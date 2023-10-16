40a086a6f844aa3ae759e39ca9e566f1038426d8
Aditi Jain <aditijain7836@gmail.com>
2023-10-16 19:17:55 +0530

    TDD for Tic Tac Toe

---
+++ src/App.spec.js
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/react";
+import App from "./App";
+
+describe("tic-tac-toe game being rendered", () => {
+  test("renders the status", () => {
+    const { container } = render(<App />);
+    expect(container.getElementsByClassName("status").length).toBe(1);
+  });
+
+  test("renders the board rows", () => {
+    const { container } = render(<App />);
+    expect(container.getElementsByClassName("board-row").length).toBe(3);
+  });
+
+//   test("renders the game board ", () => {
+//     const { container } = render(<App />);
+//     expect(container.getElementsByClassName("game-board").length).toBe(1);
+//   });
+});
